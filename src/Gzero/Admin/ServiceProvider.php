b3109cf4cdd341e6121d9f000663383d71d07b5e
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-10-13 20:16:47 +0200

    Add service provider

---
+++ src/Gzero/Admin/ServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Gzero\Admin;
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class ServiceProvider
+ * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
+ * @copyright  Copyright (c) 2014, Adrian Skierniewski
+ */
+class ServiceProvider {
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    private function registerRoutes()
+    {
+        require_once __DIR__ . '/../routes.php';
+    }
+} 
