f13bf3cdaee98470501d9367517a0e610cca0e9f
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2014-11-10 14:37:27 +0100

    More work on content details

--- src/app/src/filters/filters.js
+++ src/app/src/filters/filters.js
@@ -4,4 +4,13 @@ angular.module('filters', []).filter('langName', function() {
     return function(langCode) {
         return 'LANG_NAME_' + angular.uppercase(langCode);
     };
+}).filter('isActive', function() {
+    'use strict';
+    // Returns translatable string based on provided language code
+    return function(isActive, button) {
+        if (button) {
+            return isActive ? 'HOLD_PUBLICATION' : 'PUBLISH';
+        }
+        return isActive ? 'PUBLISHED' : 'NOT_PUBLISHED';
+    };
 });
