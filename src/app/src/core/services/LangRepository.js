7c0d1fff99937af6dde706ce17eaf2ae945c8f57
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-11-14 11:57:06 +0100

    Extended Restangular langs model

--- src/app/src/core/services/LangRepository.js
+++ src/app/src/core/services/LangRepository.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function LangRepository(Restangular) {
+    /**
+     * Custom methods
+     */
+    Restangular.extendModel('langs', function(model) {
+        model.test = function() {
+            return 'test';
+        };
+        return model;
+    });
+
     var api = Restangular.all('langs');
     return {
+        one: function(code) {
+            return api.get(code);
+        },
         all: function() {
             return api.getList();
         }
