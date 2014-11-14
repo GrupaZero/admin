aceb4de14438db401ebda2314e633c9f525fb2bb
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-11-14 10:26:53 +0100

    Added admin logout action

--- src/Gzero/routes.php
+++ src/Gzero/routes.php
@@ -4,13 +4,21 @@
  * Return admin view so we can run AngularJS admin panel
  */
 Route::group(
-    ['domain' => Config::get('gzero.domain'), 'before' => 'auth'],
+    ['domain' => Config::get('gzero.domain'), 'prefix' => 'admin', 'before' => 'auth'],
     function () {
         Route::get(
-            'admin',
+            '/',
             function () {
                 \Debugbar::disable();
                 return View::make('gzero-admin::admin', ['modules' => \App::make('admin.module')]);
+            }
+        );
+
+        Route::get(
+            'logout',
+            function () {
+                Auth::logout();
+                return Redirect::route('home');
             }
         );
     }
