e8f0e02bf5ee22779a9b8ba6f5aa971a56e17f91
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2014-12-10 19:38:19 +0100

    Notifications service refactor

--- src/app/src/core/services/Notifications.js
+++ src/app/src/core/services/Notifications.js
@@ -2,28 +2,59 @@
 
 function Notifications($alert, $translate) {
     var container = '.main';
+    /**
+     * Function which shows messages of given type
+     *
+     * @param callback function used to show each message
+     * @param messages messages to show
+     */
+    var addMessages = function(callback, messages) {
+        _.forEach(messages, function(messages) {
+            callback(messages[0]);
+        });
+    };
     return {
+        /**
+         * Function shows multiple AngularStrap info type alerts
+         *
+         * @param messages translatable messages to show
+         */
         addInfos: function(messages) {
             var self = this;
-            self.addMessages(self.addInfo, messages);
+            addMessages(self.addInfo, messages);
         },
+        /**
+         * Function shows multiple AngularStrap danger type alerts
+         *
+         * @param messages translatable messages to show
+         */
         addErrors: function(messages) {
             var self = this;
-            self.addMessages(self.addError, messages);
+            addMessages(self.addError, messages);
         },
+        /**
+         * Function shows multiple AngularStrap warning type alerts
+         *
+         * @param messages translatable messages to show
+         */
         addWarnings: function(messages) {
             var self = this;
-            self.addMessages(self.addWarning, messages);
+            addMessages(self.addWarning, messages);
         },
+        /**
+         * Function shows multiple AngularStrap success type alerts
+         *
+         * @param messages translatable messages to show
+         */
         addSuccesses: function(messages) {
             var self = this;
-            self.addMessages(self.addSuccess, messages);
+            addMessages(self.addSuccess, messages);
         },
-        addMessages: function(callback, messages) {
-            _.forEach(messages, function(messages) {
-                callback(messages[0]);
-            });
-        },
+        /**
+         * Function shows the AngularStrap info type alert
+         *
+         * @param message translatable message string eg. 'COMMON_ERROR'
+         */
         addInfo: function(message) {
             $alert({
                 title: $translate.instant('INFORMATION') + ':',
@@ -32,6 +63,11 @@ function Notifications($alert, $translate) {
                 type: 'info'
             });
         },
+        /**
+         * Function shows the AngularStrap danger type alert
+         *
+         * @param message translatable message string eg. 'COMMON_ERROR'
+         */
         addError: function(message) {
             $alert({
                 title: $translate.instant('ERROR') + ':',
@@ -40,6 +76,11 @@ function Notifications($alert, $translate) {
                 type: 'danger'
             });
         },
+        /**
+         * Function shows the AngularStrap warning type alert
+         *
+         * @param message translatable message string eg. 'COMMON_ERROR'
+         */
         addWarning: function(message) {
             $alert({
                 title: $translate.instant('WARNING') + ':',
@@ -48,6 +89,11 @@ function Notifications($alert, $translate) {
                 type: 'warning'
             });
         },
+        /**
+         * Function shows the AngularStrap success type alert
+         *
+         * @param message translatable message string eg. 'COMMON_ERROR'
+         */
         addSuccess: function(message) {
             $alert({
                 title: $translate.instant('SUCCESS') + ':',
