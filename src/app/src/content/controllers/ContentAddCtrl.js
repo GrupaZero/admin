8327f3d63951b40ce5acea34220ebe751af2285a
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2014-12-23 20:27:22 +0100

    Content add fix

--- src/app/src/content/controllers/ContentAddCtrl.js
+++ src/app/src/content/controllers/ContentAddCtrl.js
@@ -3,9 +3,8 @@
 function ContentAddCtrl($scope, $state, $stateParams, ContentRepository) {
     // contents POST action
     $scope.addNewContent = function addNewContent(newContent) {
-        newContent.langCode = $scope.listLang.code;
+        _.merge(newContent, {'translations_langCode': $scope.listLang.code});
         newContent.type = $stateParams.type;
-        newContent.isActive = 1;
         ContentRepository.newContent(newContent).then(function(response) {
             $state.go('content.list', {contentId: response.id}, {reload: true});
         });
