d2142297915ba8f07c1a41bd3e1ed21510fdc0d6
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2014-12-24 15:58:54 +0100

    Content add another fix, and css overflow fix

--- src/app/src/content/controllers/ContentAddCtrl.js
+++ src/app/src/content/controllers/ContentAddCtrl.js
@@ -3,7 +3,7 @@
 function ContentAddCtrl($scope, $state, $stateParams, ContentRepository) {
     // contents POST action
     $scope.addNewContent = function addNewContent(newContent) {
-        _.merge(newContent, {'translations_langCode': $scope.listLang.code});
+        newContent.translations.langCode = $scope.listLang.code;
         newContent.type = $stateParams.type;
         ContentRepository.newContent(newContent).then(function(response) {
             $state.go('content.list', {contentId: response.id}, {reload: true});
