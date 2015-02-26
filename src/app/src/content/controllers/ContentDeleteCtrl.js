be748f105082b4c0c6e7a686b0dc2719adb0ceed
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2015-02-26 18:49:44 +0100

    Docs

--- src/app/src/content/controllers/ContentDeleteCtrl.js
+++ src/app/src/content/controllers/ContentDeleteCtrl.js
@@ -3,7 +3,12 @@
 function ContentDeleteCtrl($scope, $state, $modal, ContentRepository) {
     var vm = this;
     var viewPath = 'packages/gzero/admin/views/content/directives/';
+    // Delete modal
     vm.deleteModal = {
+        /**
+         * Function initiates the AngularStrap modal
+         *
+         */
         initModal: function() {
             var self = this;
             self.modal = $modal({
@@ -15,20 +20,33 @@ function ContentDeleteCtrl($scope, $state, $modal, ContentRepository) {
                 placement: 'center'
             });
         },
+        /**
+         * Function shows the AngularStrap modal
+         *
+         * @param contentId content id to be removed, it is saved in the scope
+         */
         showModal: function(contentId) {
             var self = this;
             vm.contentId = contentId;
             self.initModal();
         },
+        /**
+         * Function close the modal
+         *
+         */
         closeModal: function() {
             var self = this;
             self.modal.hide();
         },
-        // contents DELETE action
+        /**
+         * Function performs the RestAngular DELETE action for content id in scope
+         *
+         */
         deleteContent: function() {
             var self = this;
             ContentRepository.deleteContent(vm.contentId).then(function(response) {
                 self.closeModal();
+                // refresh current state
                 $state.go($state.current, {}, {reload: true});
             });
         }
