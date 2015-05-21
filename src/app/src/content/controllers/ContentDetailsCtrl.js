fe6c517bc5cf86787cec10140f579de95f6ef478
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-05-21 17:21:39 +0200

    KMS-45_edit_basic_content_field

--- src/app/src/content/controllers/ContentDetailsCtrl.js
+++ src/app/src/content/controllers/ContentDetailsCtrl.js
@@ -1,15 +1,19 @@
 'use strict';
 
-function ContentDetailsCtrl($scope, $stateParams, ContentRepository) {
+function ContentDetailsCtrl($scope, $stateParams, ContentRepository, Notifications) {
     // get single content
     ContentRepository.one($stateParams.contentId).then(function(response) {
         $scope.content = ContentRepository.clean(response);
     });
 
-    $scope.saveContent = function(){
+    $scope.saveContent = function() {
         ContentRepository
+            .updateContent($scope.content.id, $scope.content)
+            .then(function() {
+                Notifications.addSuccess('SAVED');
+            });
     };
 
 }
-ContentDetailsCtrl.$inject = ['$scope', '$stateParams', 'ContentRepository'];
+ContentDetailsCtrl.$inject = ['$scope', '$stateParams', 'ContentRepository', 'Notifications'];
 module.exports = ContentDetailsCtrl;
