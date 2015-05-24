c3addfaecde6dffda204edfe8e4feb7950c4aba1
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-05-24 13:20:27 +0200

    KMS-76_fix_user_edit

--- src/app/src/user/controllers/UserEditCtrl.js
+++ src/app/src/user/controllers/UserEditCtrl.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-function UserDetailsCtrl($scope, $stateParams, UserRepository, Notifications) {
+function UserDetailsCtrl($scope, $state, $stateParams, UserRepository, Notifications) {
     // get single user
     UserRepository.one($stateParams.userId).then(function(response) {
         $scope.user = UserRepository.clean(response);
@@ -21,10 +21,11 @@ function UserDetailsCtrl($scope, $stateParams, UserRepository, Notifications) {
 
     $scope.saveUser = function(){
         UserRepository.update($scope.user.id, $scope.user).then(function(response){
-            Notifications.addSuccess('SAVED');
+            $state.go('user.list');
+            Notifications.addSuccess('THE_CHANGES_HAVE_BEEN_SAVED');
         });
     };
 
 }
-UserDetailsCtrl.$inject = ['$scope', '$stateParams', 'UserRepository', 'Notifications'];
+UserDetailsCtrl.$inject = ['$scope', '$state','$stateParams', 'UserRepository', 'Notifications'];
 module.exports = UserDetailsCtrl;
