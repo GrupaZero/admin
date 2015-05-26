0c61cc820ee4f536569c451c16b4e7ad94d379ef
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-05-26 09:43:02 +0200

    KMS-81

--- src/Gzero/routes.php
+++ src/Gzero/routes.php
@@ -4,7 +4,7 @@
  * Return admin view so we can run AngularJS admin panel
  */
 Route::group(
-    ['domain' => Config::get('gzero.domain'), 'prefix' => 'admin', 'before' => 'restrict'],
+    ['domain' => Config::get('gzero.domain'), 'prefix' => 'admin', 'middleware' => 'App\Http\Middleware\RestrictAccess'],
     function () {
         Route::get(
             '/',
