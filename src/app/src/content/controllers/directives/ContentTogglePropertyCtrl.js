eac68d5ccecc65ef1558823c6942d535abedb568
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2015-11-06 02:29:19 +0100

    KMS-97_edition_without_mask

---
+++ src/app/src/content/controllers/directives/ContentTogglePropertyCtrl.js
@@ -0,0 +1,36 @@
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class ContentTogglePropertyCtrl
+ *
+ * @package    Admin
+ * @author     Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
+ * @copyright  Copyright (c) 2015, Mateusz Urbanowicz
+ */
+
+'use strict';
+
+function ContentTogglePropertyCtrl(Utils, ContentRepository) {
+    var vm = this;
+
+    vm.toggleProperty = {
+
+        toggleProperty: function(contentId, propertyName, currentValue) {
+            var newValue = !currentValue;
+            var content = {};
+            content[propertyName] = newValue;
+            ContentRepository.updateContent(contentId, content).then(
+                function() {
+                    Utils.$state.reload();
+                }
+            );
+        }
+
+    };
+
+}
+ContentTogglePropertyCtrl.$inject = ['Utils', 'ContentRepository'];
+module.exports = ContentTogglePropertyCtrl;
---
+++ src/app/src/content/directives/ContentWeightEditButton.js
@@ -0,0 +1,35 @@
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class ContentWeightEditButton
+ *
+ * @package    Admin
+ * @author     Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
+ * @copyright  Copyright (c) 2015, Mateusz Urbanowicz
+ */
+
+'use strict';
+
+function ContentWeightEditButton() {
+    return {
+        scope: true,
+        restrict: 'A',
+        controller: 'ContentWeightEditCtrl',
+        controllerAs: 'vm',
+        bindToController: true,
+        link: function(scope, element, attrs, ContentWeightEditCtrl) {
+            element.on('click', function() {
+                ContentWeightEditCtrl.editModal.showModal(
+                    attrs.contentId,
+                    parseInt(attrs.contentWeight)
+                );
+            });
+        }
+    };
+}
+
+ContentWeightEditButton.$inject = [];
+module.exports = ContentWeightEditButton;
