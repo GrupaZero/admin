607a4d8424dd07f801db8b36e6e86f469dcf5eba
Aleksandra Augustyn <a.augustyn@grupazero.pl>
2015-11-25 18:09:53 +0100

    Translating content without parent translations bug KMS-170

--- src/app/src/content/controllers/ContentAddTranslationCtrl.js
+++ src/app/src/content/controllers/ContentAddTranslationCtrl.js
@@ -1,33 +1,42 @@
 'use strict';
 
-function ContentAddTranslationCtrl($scope, Utils, PreviousState, ContentRepository) {
-    $scope.showTeaser = false;
-
+function ContentAddTranslationCtrl($scope, Utils, content, ContentRepository) {
     $scope.ckOptions = Utils.ckOptions;
+    $scope.isLoaded = true; // form visibility
 
     // default translations lang code
     $scope.newContentTranslation = {
         contentId: Utils.$stateParams.contentId,
         langCode: Utils.$stateParams.langCode
     };
+
+    // if parent category exists
+    if (content.parentId !== null) {
+        $scope.isLoaded = false; // hide form
+        // get parent category
+        ContentRepository.one(content.parentId).then(function(response) {
+            var parent = ContentRepository.clean(response);
+            // check for route translation in selected language
+            var route = _.pluck(_.filter(parent.route.translations, 'lang', $scope.newContentTranslation.langCode), 'url');
+            if (!route.length) {
+                // Redirect user to previous state or content list
+                Utils.redirectBack('content.list');
+                // "Before adding translations to this page, you need to translate the categories in which it is located!"
+                Utils.Notifications.addInfo('NO_PARENT_TRANSLATION_ERROR');
+            } else {
+                // parent url is translated, show form
+                $scope.isLoaded = true;
+            }
+        });
+    }
+
     // contents POST action
     $scope.addnewContentTranslation = function addNewContent(newContentTranslation) {
         ContentRepository.newContentTranslation(Utils.$stateParams.contentId, newContentTranslation).then(function(response) {
-
-            try {
-                if (PreviousState.url.length > 0) {
-                    // redirected back to the state we came from
-                    Utils.$state.go(PreviousState.name, PreviousState.params, {reload: true});
-                } else {
-                    // otherwise go to content list
-                    Utils.$state.go('content.list', {}, {reload: true});
-                }
-            } catch (exception) { // if PreviousState is not resolved we still want to go back to list
-                Utils.$state.go('content.list', {}, {reload: true});
-            }
-
+            // Redirect user to previous state or content list
+            Utils.redirectBack('content.list');
         });
     };
 }
-ContentAddTranslationCtrl.$inject = ['$scope', 'Utils', 'PreviousState', 'ContentRepository'];
+ContentAddTranslationCtrl.$inject = ['$scope', 'Utils', 'content', 'ContentRepository'];
 module.exports = ContentAddTranslationCtrl;
--- src/app/src/core/services/Utils.js
+++ src/app/src/core/services/Utils.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Utils(Notifications, Storage, $state, $stateParams, ckOptions) {
+function Utils(Notifications, Storage, $state, $previousState, $stateParams, $timeout, ckOptions) {
 
     return {
         Notifications: Notifications,
@@ -8,10 +8,29 @@ function Utils(Notifications, Storage, $state, $stateParams, ckOptions) {
         $state: $state,
         $stateParams: $stateParams,
         Config: Config,
-        ckOptions: ckOptions
+        ckOptions: ckOptions,
+        /**
+         * Redirect user to previous state
+         * @param {string} defaultStateName default state name
+         */
+        redirectBack: function(defaultStateName) {
+            // Gets a reference to the previous state.
+            var previousState = $previousState.get();
+            // Wait for CKEeditor instance, so that it can be removed without errors
+            $timeout(function() {
+                // if there is a previousState
+                if (previousState !== null) {
+                    // redirected back to the state we came from
+                    $state.go(previousState.state.name, previousState.params, {reload: true});
+                } else {
+                    // otherwise go to default state
+                    $state.go(defaultStateName, {}, {reload: true});
+                }
+            }, 100);
+        }
     };
 
 }
 
-module.$inject = ['Notifications', 'Storage', '$state', '$stateParams', 'ckOptions'];
+module.$inject = ['Notifications', 'Storage', '$state', '$stateParams', '$timeout', 'ckOptions'];
 module.exports = Utils;
