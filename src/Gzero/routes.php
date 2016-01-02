051aa2e1eb5d3b9b6e33e54b47259cf96d583eab
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2016-01-02 15:30:50 +0100

    Admin as named route

--- src/Gzero/routes.php
+++ src/Gzero/routes.php
@@ -8,12 +8,15 @@ group(
     function () {
         get(
             '/',
-            function () {
-                if (class_exists('Debugbar')) {
-                    \Debugbar::disable();
+            [
+                'as' => 'admin',
+                function () {
+                    if (class_exists('Debugbar')) {
+                        \Debugbar::disable();
+                    }
+                    return view('gzero-admin::admin', ['modules' => app()->make('admin.module')]);
                 }
-                return view('gzero-admin::admin', ['modules' => app()->make('admin.module')]);
-            }
+            ]
         );
 
         get(
