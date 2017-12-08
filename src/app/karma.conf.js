37c2a1831f84952d43d00a2f7273e381026fc59b
Michał <michasstrzelecki@gmail.com>
2017-12-08 15:41:24 +0100

    Change google analytics id label to google tag manager id. (#91)
    * Change google analytics id label to google tag manager id.
    
    * Update to node to install npm package.
    
    * Try to run firefox in karma tests.
    
    * Try to run karma.
    
    * Try to run karma.
    
    * changes to karma config
    
    * try to pass tests on firefox
    
    * Try to pass tests.
    
    * Increase log level.
    
    * Update karma.
    
    * Change log level to info.

--- src/app/karma.conf.js
+++ src/app/karma.conf.js
@@ -51,6 +51,9 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Firefox', 'PhantomJS'],
 
+        // default 10000
+        browserNoActivityTimeout: 30000,
+
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
